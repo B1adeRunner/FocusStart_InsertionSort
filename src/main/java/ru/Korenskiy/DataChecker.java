e2edaefe217cb87c8848d8f03eb3ad2088f51906
LeX LuTHoR <lex_luthor_2006@inbox.ru>
2016-09-11 20:12:36 +0700

    fixed checking of completeness of initial information

--- src/main/java/ru/Korenskiy/DataChecker.java
+++ src/main/java/ru/Korenskiy/DataChecker.java
@@ -1,27 +1,14 @@
 package ru.Korenskiy;
 
 public class DataChecker {
-    private String sourceFileName;
-    private String targetFileName;
-    private String sortMode;
+    private String[] data;
 
     public DataChecker(String[] data){
-        if(data.length != 0) {
-            sourceFileName = data[0];
-            targetFileName = data[1];
-            sortMode = data[2];
-        } else {
-            System.out.println("Вы не ввели необходимые данные для запуска программы:\n" +
-                    "in.txt        имя исходного файла\n" +
-                    "out.txt       имя вывода информации\n" +
-                    "-a/-d         порядок соритровки, в возрастающем/убывающем порядке\n" +
-                    "Пример команды для запуска:\njava -jar InsertionSort-1.0-SNAPSHOT.jar in.txt out.txt -a");
-            Thread.currentThread().stop();
-        }
+        this.data = data;
     }
 
     public void targetFileNameControl() {
-        if (targetFileName.equals("out.txt")) {
+        if (data[1].equals("out.txt")) {
 
         } else {
             System.out.println("Вы ввели не верное имя файла вывода информации. Верное имя:\n out.txt");
@@ -30,7 +17,7 @@ public class DataChecker {
     }
 
     public void sourceFileNameControl(){
-        if(sourceFileName.equals("in.txt")){
+        if(data[0].equals("in.txt")){
 
         } else {
             System.out.println("Вы ввели не верное имя исходного файла информации. Верное имя:\n in.txt");
@@ -39,11 +26,22 @@ public class DataChecker {
     }
 
     public void sortModeControl(){
-        if((sortMode.equals("-a")) | (sortMode.equals("-d"))) {
+        if((data[2].equals("-a")) | (data[2].equals("-d"))) {
 
         } else {
             System.out.println("Вы ввели не верное обозначение режима сортировки (на англ. яз):\n -a     в возрастающем порядке" +
                     "\n -d     в убывающем порядке");
+            Thread.currentThread().stop();
+        }
+    }
+
+    public void informationCompletenessControl(){
+        if(data.length != 3) {
+            System.out.println("Вы ввели неверные данные для запуска программы:\n" +
+                    "in.txt        имя исходного файла\n" +
+                    "out.txt       имя вывода информации\n" +
+                    "-a/-d         порядок соритровки, в возрастающем/убывающем порядке\n" +
+                    "Пример команды для запуска:\njava -jar InsertionSort-1.0-SNAPSHOT.jar in.txt out.txt -a");
             Thread.currentThread().stop();
         }
     }
--- src/main/java/ru/Korenskiy/Main.java
+++ src/main/java/ru/Korenskiy/Main.java
@@ -3,6 +3,7 @@ package ru.Korenskiy;
 public class Main {
     public static void main( String[] args ) {
         DataChecker dataChecker = new DataChecker(args);
+        dataChecker.informationCompletenessControl();
         dataChecker.sortModeControl();
         dataChecker.sourceFileNameControl();
         dataChecker.targetFileNameControl();
